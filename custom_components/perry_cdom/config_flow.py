e63f55dac8ca5a10f835bb55e3fd24f730129ce8
ivancoppa <ivan.coppa@gmail.com>
2024-10-26 06:40:23 +0200

    Publish to Hacs (#4)
    * feat: integration for hacs
    
    * fix: minor changes

--- custom_components/perry_cdom/config_flow.py
+++ custom_components/perry_cdom/config_flow.py
@@ -5,30 +5,20 @@ from __future__ import annotations
 from collections.abc import Mapping
 from typing import Any, cast
 
-import voluptuous as vol
-
-from homeassistant.components import fan, switch
-from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN, SensorDeviceClass
 from homeassistant.const import CONF_NAME, DEGREE
 from homeassistant.helpers import selector
 from homeassistant.helpers.schema_config_entry_flow import (
     SchemaConfigFlowHandler,
     SchemaFlowFormStep,
 )
+import voluptuous as vol
 
-from .const import (
-    #CONF_AC_MODE,
-    CONF_COLD_TOLERANCE,
-    CONF_HEATER,
-    CONF_HOT_TOLERANCE,
-    CONF_MIN_DUR,
-    CONF_PRESETS,
-    CONF_SENSOR,
-    DEFAULT_TOLERANCE,
-    CONF_CDOM_SERIAL_NUMBER,
+from .const import (  # CONF_AC_MODE,
     CONF_CDOM_PIN,
-    CONF_SCAN_INTERVAL_SECONDS,
     CONF_CDOM_SCAN_INTERVAL,
+    CONF_CDOM_SERIAL_NUMBER,
+    CONF_PRESETS,
+    CONF_SCAN_INTERVAL_SECONDS,
     DOMAIN,
 )
 
@@ -45,13 +35,14 @@ OPTIONS_SCHEMA = {
             autocomplete="Pin",
         )
     ),
-
-    vol.Optional(CONF_CDOM_SCAN_INTERVAL, default=CONF_SCAN_INTERVAL_SECONDS): selector.NumberSelector(
+    vol.Optional(
+        CONF_CDOM_SCAN_INTERVAL, default=CONF_SCAN_INTERVAL_SECONDS
+    ): selector.NumberSelector(
         selector.NumberSelectorConfig(
             mode=selector.NumberSelectorMode.BOX,
-            unit_of_measurement="Seconds", 
+            unit_of_measurement="Seconds",
             step=1,
-            min=10
+            min=10,
         )
     ),
 }
@@ -70,23 +61,19 @@ CONFIG_SCHEMA = {
     **OPTIONS_SCHEMA,
 }
 
-CONFIG_FLOW = {
-    "user": SchemaFlowFormStep(vol.Schema(CONFIG_SCHEMA))
-}
+CONFIG_FLOW = {"user": SchemaFlowFormStep(vol.Schema(CONFIG_SCHEMA))}
 
-OPTIONS_FLOW = {
-    "init": SchemaFlowFormStep(vol.Schema(OPTIONS_SCHEMA))
-}
+OPTIONS_FLOW = {"init": SchemaFlowFormStep(vol.Schema(OPTIONS_SCHEMA))}
 
-#CONFIG_FLOW = {
+# CONFIG_FLOW = {
 #    "user": SchemaFlowFormStep(vol.Schema(CONFIG_SCHEMA), next_step="presets"),
 #    "presets": SchemaFlowFormStep(vol.Schema(PRESETS_SCHEMA)),
-#}
+# }
 
-#OPTIONS_FLOW = {
+# OPTIONS_FLOW = {
 #    "init": SchemaFlowFormStep(vol.Schema(OPTIONS_SCHEMA), next_step="presets"),
 #    "presets": SchemaFlowFormStep(vol.Schema(PRESETS_SCHEMA)),
-#}
+# }
 
 
 class ConfigFlowHandler(SchemaConfigFlowHandler, domain=DOMAIN):
